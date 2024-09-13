0aa1270da51a3cc98d3b0d875f3486110a74e09c
gihpee <gihpeee@gmail.com>
2024-09-13 23:17:22 +0300

    add urls

--- avito_test/avito_test/settings.py
+++ avito_test/avito_test/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure--^-2*04&$@+15^ocpkh0s9_9l#9jy&%*@a71^jjo84-oinh997
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- avito_test/avito_test/urls.py
+++ avito_test/avito_test/urls.py
@@ -17,9 +17,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from service.views import Ping
+from service.views import Ping, GetTender, CreateTender, GetUserTenders, TenderStatus, EditTender, RollbackTender, \
+    CreateBid, UserBids, TenderBids, BidStatus, EditBid, SubmitDecision, SendFeedback, RollbackBid, GetFeedback
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/ping/', Ping.as_view(), name='ping'),
+    path('api/tenders/', GetTender.as_view(), name='tenders'),
+    path('api/tenders/new/', CreateTender.as_view(), name='tenders-new'),
+    path('api/tenders/my/', GetUserTenders.as_view(), name='tenders-my'),
+    path('api/tenders/<uuid:tenderId>/status/', TenderStatus.as_view(), name='tender-status'),
+    path('api/tenders/<uuid:tenderId>/edit/', EditTender.as_view(), name='tender-edit'),
+    path('api/tenders/<uuid:tenderId>/rollback/<int:version>/', RollbackTender.as_view(), name='tender-rollback'),
+    path('api/bids/new/', CreateBid.as_view(), name='bid-new'),
+    path('api/bids/my/', UserBids.as_view(), name='bid-my'),
+    path('api/bids/<uuid:tenderId>/list/', TenderBids.as_view(), name='bid-list'),
+    path('api/bids/<uuid:bidId>/status/', BidStatus.as_view(), name='bid-status'),
+    path('api/bids/<uuid:bidId>/edit/', EditBid.as_view(), name='bid-edit'),
+    path('api/bids/<uuid:bidId>/submit-decision/', SubmitDecision.as_view(), name='bid-submit'),
+    path('api/bids/<uuid:bidId>/feedback/', SendFeedback.as_view(), name='bid-feedback'),
+    path('api/bids/<uuid:bidId>/rollback/<int:version>/', RollbackBid.as_view(), name='bid-rollback'),
+    path('api/bids/<uuid:tenderId>/reviews/', GetFeedback.as_view(), name='get-feedback'),
 ]
